e39cdbc1534807cf1181adb9cfddf29f6308c2c2
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-03-12 16:34:55 +0530

    added order service to reStruct

---
+++ OrderService/src/main/java/com/example/orderService/Model/Order.java
@@ -0,0 +1,27 @@
+package com.example.orderService.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList = new java.util.ArrayList<>();
+
+}
