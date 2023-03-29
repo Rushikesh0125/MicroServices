7489b750c9d96797e84e398e7a573dcb53ee45fc
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-03-29 16:37:09 +0530

    added custom lookup trace to order service

--- OrderService/src/main/java/com/example/orderService/Controller/OrderController.java
+++ OrderService/src/main/java/com/example/orderService/Controller/OrderController.java
@@ -3,10 +3,14 @@ package com.example.orderService.Controller;
 import com.example.orderService.Service.OrderService;
 import com.example.orderService.dto.OrderRequest;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import io.netty.util.concurrent.CompleteFuture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("api/order")
@@ -19,12 +23,12 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     @TimeLimiter(name = "inventory")
-    public String createOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "order created successfully";
+    @Retry(name = "inventory")
+    public CompletableFuture<String> createOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
 
-    public String fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "OOpsss! something went wrong";
+    public CompletableFuture<String> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() -> "OOpsss! something went wrong");
     }
 }
